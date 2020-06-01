f09556d9716ca8776042f2b8b11f11d95b4d1b20
Girija Joshi <girija_aj@yahoo.com>
2020-06-01 15:36:13 -0700

    cleaning student data

---
+++ my_module.py
@@ -0,0 +1,17 @@
+"""
+Our first Python module. This initial string is a module-level documentation string.
+It is not a necessary component of the module. It is a useful way to describe the
+purpose of your module.
+"""
+
+print("I am being executed!")
+
+some_list = ["a", 1, None]
+
+
+def square(x):
+    return x ** 2
+
+
+def cube(x):
+    return x ** 3
